49207e4dc010d866125ea4b333de899d8cb84339
daichi0918 <kayasugadaichi@icloud.com>
2023-02-26 09:23:08 +0900

    edit:user_controller_spec.ts

--- nestjs-typeorm-example/src/users/users.controller.spec.ts
+++ nestjs-typeorm-example/src/users/users.controller.spec.ts
@@ -1,20 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+// describe('UsersController', () => {
+//   let controller: UsersController;
+
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       controllers: [UsersController],
+//       providers: [UsersService],
+//     }).compile();
+
+//     controller = module.get<UsersController>(UsersController);
+//   });
+
+//   it('should be defined', () => {
+//     expect(controller).toBeDefined();
+//   });
+// });
+
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create()', () => {
+    it('should create a user', () => {
+      const dto: CreateUserDto = {
+        name: '太郎',
+      };
+
+      jest
+        .spyOn(service, 'create')
+        .mockImplementation(async (dto: CreateUserDto) => {
+          const user: User = {
+            id: 1,
+            ...dto,
+          };
+          return user;
+        });
+
+      expect(controller.create(dto)).resolves.toEqual({
+        id: 1,
+        ...dto,
+      });
+    });
   });
-});
+})
--- nestjs-typeorm-example/src/users/users.service.spec.ts
+++ nestjs-typeorm-example/src/users/users.service.spec.ts
@@ -1,18 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { UsersController } from './users.controller';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+// describe('UsersService', () => {
+//   let service: UsersService;
+
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       providers: [UsersService],
+//     }).compile();
+
+//     service = module.get<UsersService>(UsersService);
+//   });
+
+//   it('should be defined', () => {
+//     expect(service).toBeDefined();
+//   });
+// });
 
 describe('UsersService', () => {
   let service: UsersService;
-
   beforeEach(async () => {
+    // テスト用のモジュールを作成する
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
+    // テスト用のモジュールから、UsersServiceを取得する
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  // テストケース
+  describe('create()', () => {
+    it('should successfully insert a user', () => {
+      const dto: CreateUserDto = {
+        name: '太郎',
+      };
+
+      jest
+        .spyOn(service, 'create')
+        .mockImplementation(async (dto: CreateUserDto) => {
+          const user: User = {
+            id: 1,
+            ...dto,
+          };
+          return user;
+        });
+
+      expect(service.create(dto)).resolves.toEqual({
+        id: 1,
+        ...dto,
+      });
+    });
   });
 });
