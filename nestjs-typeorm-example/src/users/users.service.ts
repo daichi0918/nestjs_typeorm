e9f3063a87a73aa2e2c1b299158baf14fcb7c92a
daichi0918 <kayasugadaichi@icloud.com>
2023-02-26 09:10:27 +0900

    add post method to service

--- nestjs-typeorm-example/src/users/users.service.ts
+++ nestjs-typeorm-example/src/users/users.service.ts
@@ -1,12 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) { }
+
+  async create({ name }: CreateUserDto): Promise<User> {
+    return await this.userRepository
+      .save({
+        name: name,
+      })
+      .catch((e) => {
+        throw new InternalServerErrorException(
+          `[${e.message}]：ユーザーの登録に失敗しました。`,
+        );
+      });
   }
+
+  // create(createUserDto: CreateUserDto) {
+  //   return 'This action adds a new user';
+  // }
 
   findAll() {
     return `This action returns all users`;
